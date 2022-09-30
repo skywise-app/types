7d60c731a83d1682058125704ad8b6d15e7c72f0
John Long <kiino@qq.com>
2022-09-30 15:41:13 -0700

    0.3.0

--- api.ts
+++ api.ts
@@ -1,18 +1,18 @@
 // https://google.github.io/styleguide/jsoncstyleguide.xml
 // https://jsonapi.org/format/#document-structure
 
-interface JsonApiBase {
+interface JsonApiResponse {
     version?: number;
     meta?: Record<string, any>;
 }
 
-export interface JsonApiResponse extends JsonApiBase {
+export interface JsonApiSuccessfulResponse extends JsonApiResponse {
     data: Record<any, any> | Record<any, any>[];
 }
 
-export interface JsonApiError extends JsonApiBase {
+export interface JsonApiFailedResponse extends JsonApiResponse {
     error: {
-        code?: number;
+        code: number;
         title: string;
         detail?: string;
     };
@@ -20,14 +20,24 @@ export interface JsonApiError extends JsonApiBase {
 
 export class JsonApiError extends Error {
     public name: string = 'JsonApiError';
-    public code: JsonApiError['error']['code'];
-    public title: JsonApiError['error']['title'];
-    public detail: JsonApiError['error']['detail'];
+    public code: JsonApiFailedResponse['error']['code'];
+    public title: JsonApiFailedResponse['error']['title'];
+    public detail: JsonApiFailedResponse['error']['detail'];
 
-    constructor(errorDetail: JsonApiError) {
+    constructor(errorDetail: JsonApiFailedResponse) {
         super(errorDetail.error.title);
-        this.code = errorDetail.error.code || 500;
+        this.code = errorDetail.error.code;
         this.title = errorDetail.error.title;
         this.detail = errorDetail.error.detail || this.title;
+    }
+
+    toObj(): JsonApiFailedResponse {
+        return {
+            error: {
+                code: this.code,
+                title: this.title,
+                detail: this.detail,
+            },
+        };
     }
 }
